fa1b85d44a55fd290849936b62444b1855a3aa6d
a1trope <124525092+a1trope@users.noreply.github.com>
2023-04-18 22:17:08 +0400

    Show editor window then file in list clicked

--- Cloudocs/documents_window.py
+++ Cloudocs/documents_window.py
@@ -7,14 +7,17 @@ from editor import Editor
 
 
 class FileItem(QtWidgets.QListWidgetItem):
-    def __init__(self, fullpath):
+    def __init__(self, fullpath: str):
         super().__init__(fullpath.split('/')[-1])
+
         self.fullpath = fullpath
 
 
 class DocumentsWindow(QtWidgets.QDialog):
     def __init__(self):
-        super(QtWidgets.QDialog, self).__init__()
+        super().__init__()
+
+        self.editor_win = None
 
         # Will be initialized after call registration_window
         self.username = None
@@ -42,35 +45,44 @@ class DocumentsWindow(QtWidgets.QDialog):
 
         self.setLayout(vbox)
 
-
     def show(self):
         super().show()
         self.setWindowTitle(f"Documents list ({self.username}, id = {self.user_id})")
 
-
     def addDocumentsToList(self):
-        json_documents = rq.get("http://api.cloudocs.parasource.tech:8080" + "/api/v1/documents")
+        try:
+            # JSON objects of documents
+            json_documents = rq.get("http://api.cloudocs.parasource.tech:8080" + "/api/v1/documents")
 
-        if json_documents.status_code == 200:
-            document_names = list(map(lambda json_obj: f"{json_obj['name']} (ID = {json_obj['ID']})", json_documents.json()))
-            for name in document_names:
-                self.documentsList.addItem(name)
-
+            if json_documents.status_code == 200 and json_documents is not None:
+                # Get name of each document
+                document_names = list(
+                    map(lambda json_obj: f"{json_obj['name']} (ID = {json_obj['ID']})", json_documents.json())
+                )
+                # Adding documents to the list
+                for name in document_names:
+                    self.documentsList.addItem(FileItem(name))
+        except Exception as e:
+            print(e)
 
     def addNewDocument(self):
+        # Get filenames from QFileDialog
         filenames = QtWidgets.QFileDialog.getOpenFileNames(self)[0]
+        # Cast it to the FileItem
         list_items = [FileItem(fullpath) for fullpath in filenames]
-
+        # Add to the list
         for item in list_items:
             self.documentsList.addItem(item)
 
     def filenameClicked(self, item):
         print(f"Filename: {item.fullpath}")
+        self.editor_win = Editor()
+        self.editor_win.show()
 
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
-    
+
     docs_win = DocumentsWindow()
     docs_win.show()
 
