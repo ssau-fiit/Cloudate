097467c1889cfe5f0c3d3b8140ac3eb1d4bce487
a1trope <124525092+a1trope@users.noreply.github.com>
2023-04-18 22:15:36 +0400

    Added window with authorization errors

--- Cloudocs/registration_window.py
+++ Cloudocs/registration_window.py
@@ -4,12 +4,11 @@ import requests
 from PyQt6 import QtCore, QtGui, QtWidgets
 from documents_window import DocumentsWindow
 
-class RegistrationWindow(QtWidgets.QDialog):
 
-    session_id = None
+class RegistrationWindow(QtWidgets.QMainWindow):
 
     def __init__(self):
-        super(QtWidgets.QDialog, self).__init__()
+        super().__init__()
 
         self.setMinimumWidth(280)
         self.setMinimumHeight(150)
@@ -18,8 +17,8 @@ class RegistrationWindow(QtWidgets.QDialog):
         self.pushButton = QtWidgets.QPushButton(self)
         self.pushButton.setObjectName("pushButton")
         self.pushButton.setText("Продолжить")
-        # self.pushButton.clicked.connect(self.authenticate)
-        self.pushButton.clicked.connect(self.goToDocumentsWindow)
+        self.pushButton.clicked.connect(self.authenticate)
+        # self.pushButton.clicked.connect(self.goToDocumentsWindow)
 
         self.usernameEdit = QtWidgets.QLineEdit(self)
         self.usernameEdit.setObjectName("usernameEdit")
@@ -34,10 +33,12 @@ class RegistrationWindow(QtWidgets.QDialog):
         vbox.addWidget(self.passwordEdit)
         vbox.addWidget(self.pushButton)
 
-        self.setLayout(vbox)
+        widget = QtWidgets.QWidget()
+        widget.setLayout(vbox)
+        self.setCentralWidget(widget)
+        # self.setLayout(vbox)
 
-
-    def goToDocumentsWindow(self, user_id = -1):
+    def goToDocumentsWindow(self, user_id=-1):
         self.doc_win = DocumentsWindow()
 
         self.doc_win.username = self.usernameEdit.text()
@@ -47,33 +48,26 @@ class RegistrationWindow(QtWidgets.QDialog):
         self.doc_win.show()
         self.close()
 
-
     def authenticate(self):
         username = self.usernameEdit.text()
         password = self.passwordEdit.text()
-        
-        url = 'http://api.cloudocs.parasource.tech:8080/api/v1/auth'
-        data = {'username': username, 'password': password}
+
+        url = "http://api.cloudocs.parasource.tech:8080/api/v1/auth"
+        data = {"username": username, "password": password}
         response = requests.post(url, json = data)
 
         if response.status_code != 200:
-            # TODO: сделать показ ошибок аутентификации
-            dlg = QtWidgets.QMessageBox.critical(self, "Authentication failed!",
+            QtWidgets.QMessageBox.critical(self, "Authentication failed!",
                                                  f"Status code: {response.status_code}")
-            dlg.exec()
-            print("АУ БЛЯТЬ")
-            raise Exception('Authentication failed!')
+            return
 
         user_id = response.json()['user_id']
-
-        print(f"User ID = {user_id}")
-
         self.goToDocumentsWindow(user_id)
 
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
-    
+
     reg = RegistrationWindow()
     reg.show()
 
